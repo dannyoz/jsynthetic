7545a1cfaf648ef1342a7469a676dbf6be30ef2b
Dan Osborne <danosborne@rocketmail.com>
2016-10-18 15:01:00 +0100

    added env route

---
+++ app/components/navigation/navigation.vue
@@ -0,0 +1,19 @@
+<template>
+	<nav class="navigation">
+		<h1>{{title}}</h1>
+		<ul>
+			<li>Music</li>
+			<li>Artwork</li>
+		</ul>
+	</nav>
+</template>
+
+<script>
+	export default {
+	  data () {
+	    return {
+	      title: 'nav',
+	    }
+	  },
+	};
+</script>
