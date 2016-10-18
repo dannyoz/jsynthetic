d778a797ac610b00dbd8121908e2e2fc1c5b9314
Dan Osborne <danosborne@rocketmail.com>
2016-10-18 16:29:18 +0100

    server clean-up

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
 var express = require('express');
 var js = express();
 
-// var environment = process.env.NODE_ENV || 'development';
-// var envPath = __dirname+'/environments/'+environment+'/';
-
-// js.set('port', (process.env.PORT || 5000));
-
-// js.use(express.static(__dirname + '/public'));
-
-// // views is directory for all template files
-// js.set('views', __dirname + '/views');
-// js.set('view engine', 'ejs');
-
-// js.get('/', function(req, res) {
-//   res.setHeader('Content-Type', 'text/html');
-//   res.sendFile(envPath+'views/index.html');
-// });
-
-// js.get('/env', function(req, res) {
-// 	res.status(200).send(process.env);
-// });
-
-// js.listen(js.get('port'), function() {
-//   console.log('Node app is running on port', js.get('port'));
-// });
-
-
 var jimmySynthetic = function() {
 
     var js = this;
@@ -39,7 +14,6 @@ var jimmySynthetic = function() {
     js.initializeServer = function() {
 
         js.app = express();
-        js.app.use(express.bodyParser({limit: '50mb'}));
 
         var routes     = require('./express/routing').routes;
         var staticDirs = require('./express/routing').staticDirs;
@@ -66,6 +40,11 @@ var jimmySynthetic = function() {
         for (var route in getRoutes) {
             js.app.get(route, getRoutes[route]);
         };
+
+        // for deberging
+        js.app.get('/env', function(req, res) {
+					res.status(200).send(process.env);
+				});
 
         js.app.use(err404);
 
