ae48b665e3f5ddc1d8d49ad9229a9c16f1460d85
Dan Osborne <danosborne@rocketmail.com>
2016-10-19 09:32:41 +0100

    reverted to simpler server js

--- index.js
+++ index.js
@@ -4,45 +4,21 @@ var js = express();
 var environment = process.env.NODE_ENV || 'development';
 var envPath = __dirname+'/environments/'+environment+'/';
 
-var routing = {
-	routes : [
-        '/',
-        '/art',
-        '/music',
-        '/about'
-    ],
-	staticDirs : [
-        'css', 
-        'img', 
-        'js',
-        'icons'
-    ],
-    err404 : function(req, res, next){
 
-        var environment  = process.env.NODE_ENV || 'development',
-            envPath      = './environments/'+environment+'/';
-
-        res.status(404);
-
-        if (req.accepts('html')) {
-            return res.sendfile(envPath+'views/404.html');
-        }
-
-        if (req.accepts('json')) {
-            return res.send({ error: 'Not found' });
-        }
-
-        res.type('txt').send('Not found');
-    }
-};
+var	routes = [
+  '/',
+  '/art',
+  '/music',
+  '/about'
+];
 
 js.set('port', (process.env.PORT || 5000));
 
 routing.routes.forEach(function (path){
-    js.app.get(path,function(req,res){
-        res.setHeader('Content-Type', 'text/html');
-        res.sendfile(envPath+'views/index.html');
-    });
+  js.app.get(path,function(req,res){
+    res.setHeader('Content-Type', 'text/html');
+    res.sendfile(envPath+'views/index.html');
+  });
 });
 
 js.get('/env', function(req, res) {
