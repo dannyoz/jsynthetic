244b9ee70dbba1236446f35e0f65039e17ea1cf7
Dan Osborne <dan.osborne@rocketmail.com>
2018-08-30 22:40:04 +0100

    Added basic express api

---
+++ jimmy-synthetic/express/post-routes.js
@@ -0,0 +1,35 @@
+var nodemailer = require('nodemailer');
+var routes = {};
+var apiVersion = '/api/v1/';
+
+routes[`${apiVersion}sendmessage`] = function(req,res){
+
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: process.env.EMAIL_USER,
+			pass: process.env.EMAIL_PASSWORD
+		}
+	});
+
+	var name = req.body.name,
+		email = req.body.email,
+		message = req.body.message;
+
+	var mailOptions = {
+	    from: name+' ('+email+')',
+	    to: process.env.EMAIL_TO,
+	    subject: 'Message from Jimmy synthetic website',
+	    text: name+' ('+email+') sent the following message on the Jimmy Synthetic website: ' +message,
+	    html: '<p><b>' + name+' ('+email+')</b></p><p>Sent the following message on the Jimmy Synthetic website:</p><p>' +message + '</p>'
+	};
+
+	transporter.sendMail(mailOptions, function(err, info){
+	    if(err){
+	        res.status(500).send(err);
+	    }
+	    res.status(200).send('Message sent');
+	});
+};
+
+module.exports = routes;
